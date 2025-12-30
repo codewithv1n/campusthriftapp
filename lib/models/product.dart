d1f2f59de5401cf34d4fd830a5aef6522f0e09c4
codewithv1n <vinzelmarano@gmail.com>
2025-12-30 22:27:39 +0800

    UPDATE: minimal changes in product identification

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -2,10 +2,26 @@ class Product {
   final String name;
   final double price;
   final String description;
+  final List<String> imagePaths;
 
-  const Product({
+  Product({
     required this.name,
     required this.price,
     required this.description,
+    this.imagePaths = const [],
   });
+
+  Product copyWith({
+    String? name,
+    double? price,
+    String? description,
+    List<String>? imagePaths,
+  }) {
+    return Product(
+      name: name ?? this.name,
+      price: price ?? this.price,
+      description: description ?? this.description,
+      imagePaths: imagePaths ?? this.imagePaths,
+    );
+  }
 }
