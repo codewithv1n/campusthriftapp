af3c0dd5e0bae9898ca9735f49fd08e7ced6343a
codewithv1n <vinzelmarano@gmail.com>
2026-01-06 09:24:18 +0800

    Remove Delete Files

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,73 @@
+import 'package:campus_thrift/screens/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'screens/login_screen.dart';
 
 void main() {
   runApp(const CampusThriftApp());
 }
 
-class CampusThriftApp extends StatelessWidget {
+class CampusThriftApp extends StatefulWidget {
   const CampusThriftApp({super.key});
+
+  @override
+  State<CampusThriftApp> createState() => _CampusThriftAppState();
+}
+
+class _CampusThriftAppState extends State<CampusThriftApp> {
+  ThemeMode _themeMode = ThemeMode.light;
+
+  void _toggleTheme(bool isDark) {
+    setState(() {
+      _themeMode = isDark ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'CampusThrift',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const LoginScreen(),
-      initialRoute: '/',
+      themeMode: _themeMode,
+
+      // Light Theme
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.light,
+        colorSchemeSeed: Colors.orange,
+        scaffoldBackgroundColor: Colors.grey.shade50,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          elevation: 0,
+          centerTitle: true,
+        ),
+        cardTheme: const CardThemeData(
+          color: Colors.white,
+          elevation: 1,
+        ),
+      ),
+
+      // Dark Theme
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorSchemeSeed: Colors.orange,
+        scaffoldBackgroundColor: Colors.grey.shade900,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.grey.shade800,
+          foregroundColor: Colors.white,
+          elevation: 0,
+          centerTitle: true,
+        ),
+        cardTheme: CardThemeData(
+          color: Colors.grey.shade800,
+          elevation: 2,
+        ),
+      ),
+
+      home: HomeScreen(onThemeChanged: _toggleTheme),
+
       routes: {
-        '/login': (context) => LoginScreen(),
+        '/home': (context) => HomeScreen(onThemeChanged: _toggleTheme),
       },
     );
   }
